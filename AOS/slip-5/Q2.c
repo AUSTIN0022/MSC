dc0f1daadf62ed127e461e278f120b8baaafd98d
Austin <95069137+AUSTIN0022@users.noreply.github.com>
2024-11-04 13:22:31 +0530

    Update Q2.c

--- AOS/slip-5/Q2.c
+++ AOS/slip-5/Q2.c
@@ -1,49 +1,58 @@
 #include<stdio.h>
 #include<unistd.h>
+#include<string.h>
 
 int main()
 {
-	int pipefds[2];
-	int returnstatus;
-	int pid;
-	char msg1[20] = "Hello world", msg2[20] = "Hello SPPU", msg3[20] = "Linux is Funny";
-	char readMessage[20];
-	
-	returnstatus = pipe(pipefds);
-	
-	if(returnstatus == 1){
-		printf("Unable to create pipe 1\n");
-		return 1;
-	}
-	
-	pid = fork();
-	
-	if(pid != 0){
-		close(pipefds[0]);
-
-		printf("In parent:- Message is %s\n", msg1);
-		write(pipefds[1], msg1, sizeof(msg1));
-		sleep(1);
-		
-		printf("In parent:- Message is %s\n", msg2);
-		write(pipefds[1], msg2, sizeof(msg2));
-		sleep(1);
-		
-		printf("In parent:- Message is %s\n", msg3);
-		write(pipefds[1], msg3, sizeof(msg3));
-	} else {
-		close(pipefds[1]);
-
-		read(pipefds[0], readMessage, sizeof(readMessage));
-		printf("In child:- Message is %s\n", readMessage);
-		sleep(1);
-		
-		read(pipefds[0], readMessage, sizeof(readMessage));
-		printf("In child:- Message is %s\n", readMessage);
-		sleep(1);
-		
-		read(pipefds[0], readMessage, sizeof(readMessage));
-		printf("In child:- Message is %s\n", readMessage);
-	}
-	return 0;
+    int pipefds[2];
+    char msg1[20] = "Hello world";
+    char msg2[20] = "Hello SPPU";
+    char msg3[20] = "Linux is Funny";
+    char readMessage[20];
+    
+    if(pipe(pipefds) == -1) {
+        printf("Unable to create pipe\n");
+        return 1;
+    }
+    
+    int pid = fork();
+    
+    if(pid == -1) {
+        printf("Fork failed\n");
+        return 1;
+    }
+    
+    if(pid > 0) {  // Parent process
+        close(pipefds[1]);  // Close unused write end
+        
+        read(pipefds[0], readMessage, sizeof(readMessage));
+        printf("In parent:- Message is %s\n", readMessage);
+        sleep(1);
+        
+        read(pipefds[0], readMessage, sizeof(readMessage));
+        printf("In parent:- Message is %s\n", readMessage);
+        sleep(1);
+        
+        read(pipefds[0], readMessage, sizeof(readMessage));
+        printf("In parent:- Message is %s\n", readMessage);
+        
+        close(pipefds[0]);  // Close read end after reading
+    } 
+    else {  // Child process
+        close(pipefds[0]);  // Close unused read end
+        
+        printf("In child:- Writing Message1: %s\n", msg1);
+        write(pipefds[1], msg1, sizeof(msg1));
+        sleep(1);
+        
+        printf("In child:- Writing Message2: %s\n", msg2);
+        write(pipefds[1], msg2, sizeof(msg2));
+        sleep(1);
+        
+        printf("In child:- Writing Message3: %s\n", msg3);
+        write(pipefds[1], msg3, sizeof(msg3));
+        
+        close(pipefds[1]);  // Close write end after writing
+    }
+    return 0;
 }
