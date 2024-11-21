f35904fb1aa12cc5c13a3cf74b0f335f0f8ce5d5
Austin <95069137+AUSTIN0022@users.noreply.github.com>
2024-11-21 19:38:03 +0530

    Update Q2.c

--- AOS/slip-15/Q2.c
+++ AOS/slip-15/Q2.c
@@ -5,12 +5,11 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 
-pid_t child_pid; // Global variable to store child process ID
+pid_t child_pid;
 
-// Signal handler for SIGCHLD (child process termination)
 void chld_handler(int signum) {
     int status;
-    pid_t pid = waitpid(child_pid, &status, WNOHANG); // Non-blocking wait
+    pid_t pid = waitpid(child_pid, &status, WNOHANG);
 
     if (pid > 0) {
         if (WIFEXITED(status)) {
@@ -22,26 +21,23 @@ void chld_handler(int signum) {
     }
 }
 
-// Signal handler for SIGALRM (alarm signal)
 void alrm_handler(int signum) {
     printf("Child process did not complete within 5 seconds. Killing child process (PID: %d).\n", child_pid);
-    kill(child_pid, SIGKILL); // Kill the child process
+    kill(child_pid, SIGKILL);
 }
 
 int main() {
-    // Set up signal handler for SIGCHLD
+    
     if (signal(SIGCHLD, chld_handler) == SIG_ERR) {
         perror("signal(SIGCHLD)");
         exit(1);
     }
 
-    // Set up signal handler for SIGALRM
     if (signal(SIGALRM, alrm_handler) == SIG_ERR) {
         perror("signal(SIGALRM)");
         exit(1);
     }
 
-    // Create a child process
     child_pid = fork();
 
     if (child_pid == -1) {
@@ -49,18 +45,16 @@ int main() {
         exit(1);
     }
 
-    if (child_pid == 0) { // Child process
-        // Replace this with any command or program you want to execute
+    if (child_pid == 0) {
         printf("Child process (PID: %d) running...\n", getpid());
-        execlp("sleep", "sleep", "10", NULL); // Example command (sleep for 10 seconds)
-        perror("execlp"); // If execlp fails
+        execlp("sleep", "sleep", "10", NULL);
+        perror("execlp");
         exit(1);
-    } else { // Parent process
-        alarm(5); // Set an alarm for 5 seconds
+    } else { 
+        alarm(5); 
 
-        // Parent process waits for child process to terminate
         while (1) {
-            pause(); // Wait for any signal
+            pause(); 
         }
     }
     return 0;
